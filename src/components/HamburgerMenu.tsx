19f5521741bc6ad3fdd8462b98228eab3a1c056b
AlexPham0209 <AlexLPham0209@gmail.com>
2026-02-28 10:48:09 -0500

    Added basic hamburger menu

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,23 @@
+export function HamburgerMenu() {
+    return (
+        <ul className="hamburger-menu">
+            <li>
+                <a href="/#/home">
+                    Home
+                </a>
+            </li>
+            
+            <li>
+                <a href="/#/projects">
+                    Projects
+                </a>
+            </li>
+
+            <li>
+                <a href="/#/experience">
+                    Experience
+                </a>
+            </li>
+        </ul>
+    )
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,46 +1,39 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { HamburgerMenu } from "./HamburgerMenu";
 
 export default function NavBar({ children }: { children?: React.ReactNode }) {
-  const [isMobile, setIsMobile] = useState(false);
-  const handleResize = () => {
-    if (window.innerWidth < 720) {
-        setIsMobile(true);
-    } else {
-        setIsMobile(false);
-    }
-  }
-
-  // create an event listener
-  useEffect(() => {
-    window.addEventListener("resize", handleResize)
-  }, []);
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
   return (
-    <nav className="navbar">
-      <ul className="navbar-left">
-        <li>Alex Pham</li>
-        
-        <li>
-          <a href="Alexander-Pham-Resume.pdf">
-            <img src="resume.png"></img>
-          </a>
-        </li>
+    <div className="header">
+      <nav className="navbar">
+        <ul className="navbar-left">
+          <li>Alex Pham</li>
+          
+          <li>
+            <a href="Alexander-Pham-Resume.pdf">
+              <img src="resume.png"></img>
+            </a>
+          </li>
 
-        <li>      
-          <a href="https://github.com/AlexPham0209">
-            <img src="github.png"></img>
-          </a>
-        </li>
+          <li>      
+            <a href="https://github.com/AlexPham0209">
+              <img src="github.png"></img>
+            </a>
+          </li>
+        </ul>
 
-      </ul>
+        <ul className="navbar-right">
+          {children != null && children}
+        </ul>
 
-      <ul className="navbar-right">
-        {children != null && children}
-      </ul>
+        <button className="hamburger-menu-button" onClick={() => setHamburgerOpen(!hamburgerOpen)}>
+          <img src="hamburger.png"/>
+        </button>
+      </nav>
 
-      <button className="navbar-menu">
-        <img src="hamburger.png"/>
-      </button>
-    </nav>
+      {hamburgerOpen && <HamburgerMenu/>}
+    </div>
+      
   );
 }
